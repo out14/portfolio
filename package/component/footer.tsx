a10b77b59d89c80b579dc341ad6923cd8b215173
gima <gima@naver.com>
2026-08-24 14:45:18 +0900

    footer img link fix

--- package/component/footer.tsx
+++ package/component/footer.tsx
@@ -11,25 +11,25 @@ export const Footer = () => {
                     <ul className="footer_sns">
                         <li>
                             <a href="https://github.com/out14" target="_blank">
-                                <img src={`${import.meta.env.BASE_URL}image/git_logo.png"`} alt="깃"/>
+                                <img src={`${import.meta.env.BASE_URL}image/git_logo.png`} alt="깃"/>
                                 <span>out14</span>
                             </a>
                         </li>
                         <li>
                             <a href="" target="_blank">
-                                <img src={`${import.meta.env.BASE_URL}image/insta_logo.png"`} alt="인스타"/>
+                                <img src={`${import.meta.env.BASE_URL}image/insta_logo.png`} alt="인스타"/>
                                 <span>gi_mamaw</span> 
                             </a>
                         </li>
                         <li>
                             <a href="https://open.kakao.com/me/demalper" target="_blank">
-                                <img src={`${import.meta.env.BASE_URL}image/kakao_logo.png"`} alt="카카오"/>
+                                <img src={`${import.meta.env.BASE_URL}image/kakao_logo.png`} alt="카카오"/>
                                 <span>demalper</span>
                             </a>
                         </li>
                         <li>
                             <a href="mailto:gi_ma@naver.com">
-                                <img src={`${import.meta.env.BASE_URL}image/mail_logo.png"`} alt="메일"/>
+                                <img src={`${import.meta.env.BASE_URL}image/mail_logo.png`} alt="메일"/>
                                 <span>gi_ma@naver.com</span>
                             </a>
                         </li>
